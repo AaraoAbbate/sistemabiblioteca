9cf69fae1de8557927c9fd4edbf4ffcd27581895
AaraoAbbate <134648305+AaraoAbbate@users.noreply.github.com>
2023-09-01 17:13:58 -0300

    01/09/23

--- models/usuario.php
+++ models/usuario.php
@@ -31,7 +31,29 @@ class Usuario{
      */
     public function cadastrar($dados){
 
-    }
+'   '        try{
+            $query = "INSERT INTO {$this->table} (nome, email,senha,perfil) VALUES (:nome,:email,:senha,:perfil)";
+            $stmt = $this->db->prepare($query);
+        }catch(PDOException $e){
+            echo "Erro na preparação da consulta".$e->getMessage();
+        }
+        
+        $stmt->blindParam(':nome',$dados['nome']);
+        $stmt->blindParam(':email',$dados['email']);
+        $stmt->blindParam(':senha',$dados['senha']);
+        $stmt->blindParam(':perfil',$dados['perfil']);
+
+        try{
+            $stmt->execute();
+            echo "Inserção dem sucedida!";
+
+        }catch(PDOException $e){
+            echo"Erro na inscrição: ".$e->getMessage();
+        }
+
+        }
+
+    
     /**
      * editar usuario
      * @param int $id
@@ -40,6 +62,24 @@ class Usuario{
      */
     public function editar($id,$dados){
 
+        try{
+            $query = "UPDATE {$this->table} SET nome = :nome, email = :email, senha = :senha WHERE id_usuario = :$id";
+            $stmt = $this->db->prepare($query);
+        }catch(PDOException $e){
+            echo "erro na preparação da consulta :". $e->getMessage();
+        }
+
+        $stmt->blindParam(':nome', $dados['nome']);
+        $stmt->blindParam(':email', $dados['email']);
+        $stmt->blindParam(':senha', $dados['senha']);
+        $stmt->blindParam(':perfil', $dados['perfil']);
+
+        try{
+            $stmt->execute();
+            echo "Seus dados foram atualzados com sucesso!";
+        }catch(PDOException $e){
+            echo "Erro na inserção: ". $e->getMessage();
+        }
     }
     //excluir usuario
     public function excluir($id){
